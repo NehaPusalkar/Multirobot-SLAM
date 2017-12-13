4f2e1e3c4e9527909dc54656be05b3918b9b552f
NehaPusalkar <nehapusalkar25@gmail.com>
2017-12-13 16:55:55 +0530

    Add existing file

---
+++ multirobot/multirobot/scripts/appendtry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import numpy as np
+delj = []
+delj=np.append(delj,[2])
+print (delj)
+delj=np.append(delj,[3])
+print (delj)
+deli = []
+deli = np.append(deli,[1,2,3],axis=0)
+print (deli)
